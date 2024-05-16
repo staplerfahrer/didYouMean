546738684039dfa61e557d6c690ee60a59214b94
Jacob Bruinsma <jacob.a.bruinsma@gmail.com>
2024-05-16 15:14:35 -0500

    Update _build.cmd

--- _build.cmd
+++ _build.cmd
@@ -1,5 +1,5 @@
 @cls
 @set PATH=%PATH%;C:\MinGW\bin
-gcc.exe -g ld.c -o didYouMean.exe -O3
+gcc.exe -g main.c -o didYouMean.exe -O3
 @echo exit code %errorlevel%
 didYouMean.exe "NAMES" "FNAME" "JACOB" 2 "result12898.dbf"
